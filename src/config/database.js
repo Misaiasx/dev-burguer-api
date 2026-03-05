c2e8df75960346cc8a55ed6f18bb4c38e4e23926
misaiasx <mizaiascorrea@gmail.com>
2026-03-05 11:03:32 -0300

    05/03

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect:'postegres',
+    host: 'localhost',
+    port: 5432,
+    username: 'misaias',
+    password: '123456',
+    database: 'dev-burguer-db',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
